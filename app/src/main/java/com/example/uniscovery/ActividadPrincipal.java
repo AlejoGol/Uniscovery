3d5a5cddf8ab5a7e429843911205f6e9e1153bb9
Android Studio <devandroid@ort>
2019-05-10 10:49:22 -0300

    intento de boton

--- app/src/main/java/com/example/uniscovery/ActividadPrincipal.java
+++ app/src/main/java/com/example/uniscovery/ActividadPrincipal.java
@@ -1,7 +1,9 @@
 package com.example.uniscovery;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ActividadPrincipal extends Activity {
 
@@ -9,5 +11,20 @@ public class ActividadPrincipal extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_principal);
+    }
+    public void IrANuevaPantalla(View vista)
+    {
+        int id =vista.getId();
+        Intent PasarDePantalla;
+        if(id==R.id.BotonBuscarInformacion)
+        {
+            PasarDePantalla=new Intent(this,BuscarInformacion.class);
+            startActivity(PasarDePantalla);
+        }
+        else{
+            PasarDePantalla=new Intent(this,GenerarTest.class);
+            startActivity(PasarDePantalla);
+
+        }
     }
 }
