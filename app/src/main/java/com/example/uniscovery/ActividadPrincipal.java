df386efa900fcc026dca060fbcf3033525433a7d
Android Studio <devandroid@ort>
2019-05-10 11:39:07 -0300

    mas o menos

--- app/src/main/java/com/example/uniscovery/ActividadPrincipal.java
+++ app/src/main/java/com/example/uniscovery/ActividadPrincipal.java
@@ -2,15 +2,22 @@ package com.example.uniscovery;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.view.View;
+import android.widget.Button;
 
 public class ActividadPrincipal extends Activity {
 
+    public Button Boton1;
+    public Button Boton2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_principal);
+        Boton1=findViewById(R.id.BotonIrAlTest);
+        Boton2=findViewById(R.id.BotonBuscarInformacion);
     }
     public void IrANuevaPantalla(View vista)
     {
@@ -27,4 +34,5 @@ public class ActividadPrincipal extends Activity {
 
         }
     }
+
 }
